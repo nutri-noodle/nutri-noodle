9e74f3d99b455339e2d290a1cf49c8b774f914ce
rob mathews <rob.mathews@goodmeasures.com>
2023-09-23 06:48:52 -0400

    Sort out devise layout.

--- app/models/message.rb
+++ app/models/message.rb
@@ -7,6 +7,16 @@ class Message < ApplicationRecord
 
   belongs_to :user
 
+# explanation - since it took literally 2 days to understand this code:
+#
+# the first (unnamed) parameter is the stream name.
+# target: parameter is the id of the div in the HTML
+# important, each parameter can be either a proc (something that can be called),
+# in which case it is invoked with an ActiveRelation (which is magically user.messages)
+# or it is something else, in which case it is returned.
+
+# you can reverse engineer the code here: https://github.com/hotwired/turbo-rails/blob/main/app/models/concerns/turbo/broadcastable.rb#L101
+
   broadcasts_to(->(messages) { messages.user.dom_id(messages.user, :messages) },
                 target: ->(messages) { messages.user.dom_id(messages.user, :messages) })
 
