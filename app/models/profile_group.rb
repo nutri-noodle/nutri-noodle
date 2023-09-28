df9e3a0d78ada0d50464478b3ebdc5a97cc0b132
rob mathews <rob.mathews@goodmeasures.com>
2023-09-28 15:11:57 -0400

    fix naming

--- app/models/profile_group.rb
+++ app/models/profile_group.rb
@@ -1,4 +1,4 @@
-class ProfileCategory < ApplicationRecord
+class ProfileGroup < ApplicationRecord
   has_one :goal, dependent: :destroy
   has_many :profile_group_medical_conditions, dependent: :destroy
   validates :name, presence: true
