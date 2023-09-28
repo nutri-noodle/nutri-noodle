fc4ec5fbbfc2810646c3997c738b0e3748e378a7
rob mathews <rob.mathews@goodmeasures.com>
2023-09-28 09:01:34 -0400

    better explanation.

--- app/models/message.rb
+++ app/models/message.rb
@@ -9,8 +9,13 @@ class Message < ApplicationRecord
 
 # explanation - since it took literally 2 days to understand this code:
 #
-# the first (unnamed) parameter is the stream name.
+# the first (unnamed) parameter is the stream name. the stream name is
+#   global across the system, so you need to scope it to the user if you
+#   don't want to share data across all users.
 # target: parameter is the id of the div in the HTML
+#   I suppose it is unique to the page, not the stream, so it probably
+#   doesn't need to be scoped. unless it is automagically connected the stream
+#   name by name;)
 # important, each parameter can be either a proc (something that can be called),
 # in which case it is invoked with an ActiveRelation (which is magically user.messages)
 # or it is something else, in which case it is returned.
